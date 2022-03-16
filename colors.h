6d5b63fc8142f4c218c1d6ad393efc840974a5e4
Kamil Dziedzic <iLike135Trioxane@protonmail.com>
2022-03-16 18:31:44 +0100

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,39 +1,45 @@
 #pragma once
 
-#ifdef __linux__
-#
-#    define BLACK_FG   30
-#    define BLACK_BG   40
-#    define RED_FG     31
-#    define RED_BG     41
-#    define GREEN_FG   32
-#    define GREEN_BG   42
-#    define YELLOW_FG  33
-#    define YELLOW_BG  43
-#    define BLUE_FG    34
-#    define BLUE_BG    44
-#    define MAGENTA_FG 35
-#    define MAGENTA_BG 45
-#    define CYAN_FG    36
-#    define CYAN_BG    46
-#    define WHITE_FG   37
-#    define WHITE_BG   47
-#
-#    define BRIGHT_BLACK_FG   90
-#    define BRIGHT_BLACK_BG   100
-#    define BRIGHT_RED_FG     91
-#    define BRIGHT_RED_BG     101
-#    define BRIGHT_GREEN_FG   92
-#    define BRIGHT_GREEN_BG   102
-#    define BRIGHT_YELLOW_FG  93
-#    define BRIGHT_YELLOW_BG  103
-#    define BRIGHT_BLUE_FG    94
-#    define BRIGHT_BLUE_BG    104
-#    define BRIGHT_MAGENTA_FG 95
-#    define BRIGHT_MAGENTA_BG 105
-#    define BRIGHT_CYAN_FG    96
-#    define BRIGHT_CYAN_BG    106
-#    define BRIGHT_WHITE_FG   97
-#    define BRIGHT_WHITE_BG   107
-#
+#define RESET      0b1000000000000000
+
+#define FG_NONE    0b0000000000000000
+#define FG_BLACK   0b0000000000010000
+#define FG_RED     0b0000000000010001
+#define FG_GREEN   0b0000000000010010
+#define FG_YELLOW  0b0000000000010011
+#define FG_BLUE    0b0000000000010100
+#define FG_MAGENTA 0b0000000000010101
+#define FG_CYAN    0b0000000000010110
+#define FG_WHITE   0b0000000000010111
+#define FG_BRIGHT  0b0000000000011000
+
+#define BG_NONE    0b0000000000000000
+#define BG_BLACK   0b0000001000000000
+#define BG_RED     0b0000001000100000
+#define BG_GREEN   0b0000001001000000
+#define BG_YELLOW  0b0000001001100000
+#define BG_BLUE    0b0000001010000000
+#define BG_MAGENTA 0b0000001010100000
+#define BG_CYAN    0b0000001011000000
+#define BG_WHITE   0b0000001011100000
+#define BG_BRIGHT  0b0000001100000000
+
+#if defined(__linux__)
+
+void setColor(short clr) {
+    auto f = [](int c){ std::cout << "\033[" << c << "m"; };
+
+    if (clr & 0x8000) f( 0 );
+    if (clr & 0x0200) f( 60 * (clr & 0x100) + 40 + ((clr & 0xE0) >> 5) );
+    if (clr & 0x0010) f( 60 * (clr & 0x008) + 30 +  (clr & 0x07)       );
+}
+
+#elif defined(_WIN32)
+
+void setColor(short clr) {
+
+}
+
+#else
+void setColor(short clr) {}
 #endif
